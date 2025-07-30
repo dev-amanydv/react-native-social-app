f63a8494af8e901ad27e95a79c5ef937173523b7
Aman Yadav <ay.work07@gmail.com>
2025-07-30 20:17:59 +0530

    update: started

---
+++ app/(tabs)/bookmark.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+ 
+ export default function Bookmarks (){
+
+    return (
+        <View>
+            <Text>
+                Bokmarks page
+            </Text>
+        </View>
+    )
+ }
---
+++ app/(tabs)/notifications.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+ 
+ export default function Notifications (){
+
+    return (
+        <View>
+            <Text>
+                Notifications page
+            </Text>
+        </View>
+    )
+ }
